48a591e1b1a57680a6d5cd4d8868a5ef1b168983
Baosong <baosong@bawu-mac.local>
2016-04-28 19:23:10 +0800

    Solve 011 Container with Most Water

--- LeetCode/011_ContainerWithMostWater.h
+++ LeetCode/011_ContainerWithMostWater.h
@@ -6,11 +6,43 @@ class Solution
 public:
 	int maxArea(vector<int>& height)
 	{
-        return 1;
+        int max = -1;
+        int begin = 0;
+        int end = static_cast<int>(height.size() - 1);
+        
+        while (begin < end)
+        {
+            int min = height[begin] < height[end] ? height[begin] : height[end];
+            int water = static_cast<int>(end - begin) * min;
+            if (water > max)
+                max = water;
+            if (min == height[begin])
+            {
+                while (min >= height[++begin])
+                {
+                    
+                }
+            }
+            else
+            {
+                while (min >= height[--end])
+                {
+                    
+                }
+            }
+        }
+        
+        return max;
 	}
 };
 
 void Test()
 {
 	Solution lSolution;
+    
+    vector<int> lCase1 = {1, 2, 3};
+    assert(lSolution.maxArea(lCase1) == 2);
+    
+    vector<int> lCase2 = {1, 99, 100, 1};
+    assert(lSolution.maxArea(lCase2) == 99);
 }
