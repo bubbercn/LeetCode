5cd4c40fdc9d4f265476e920b73e590d68b784b6
Baosong Wu <bawu@microstrategy.com>
2020-01-30 14:00:44 +0800

    Solve 056 and header for 057.

--- LeetCode/056_MergeIntervals.h
+++ LeetCode/056_MergeIntervals.h
@@ -3,12 +3,57 @@
 
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        return {};
+    vector<vector<int>> merge(vector<vector<int>>& intervals)
+    {
+        sort(intervals.begin(), intervals.end());
+        for (int i = 1; i < intervals.size(); i++)
+        {
+            if (intervals[i][0] <= intervals[i - 1][1])
+            {
+                intervals[i][0] = intervals[i - 1][0];
+                if (intervals[i][1] < intervals[i - 1][1])
+                {
+                    intervals[i][1] = intervals[i - 1][1];
+                }
+                intervals[i - 1].clear();
+            }
+        }
+        
+        vector<vector<int>> result;
+        for (auto i = intervals.begin(); i != intervals.end(); i++)
+        {
+            if (!i->empty())
+            {
+                result.emplace_back(*i);
+            }
+        }
+        return result;
     }
 };
 
 void Test()
 {
+    Solution solution;
     
+    vector<vector<int>> input1 = {
+        {15,18},
+        {8,10},
+        {1,3},
+        {2,6},
+    };
+    vector<vector<int>> output1 = {
+        {1,6},
+        {8,10},
+        {15,18}
+    };
+    assert(solution.merge(input1) == output1);
+    
+    vector<vector<int>> input2 = {
+        {1,4},
+        {4,5}
+    };
+    vector<vector<int>> output2 = {
+        {1,5},
+    };
+    assert(solution.merge(input2) == output2);
 }
---
+++ LeetCode/057_InsertInterval.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Common.h"
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        return {};
+    }
+};
+
+void Test()
+{
+    
+}
