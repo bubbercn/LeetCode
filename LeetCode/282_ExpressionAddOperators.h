0c11f53f1188ee5d260829b5bce13be12f160ee4
Baosong Wu <bubbercn@163.com>
2020-08-18 08:18:01 +0800

    282 work in-progress.

--- LeetCode/282_ExpressionAddOperators.h
+++ LeetCode/282_ExpressionAddOperators.h
@@ -24,105 +24,100 @@ public:
 
         list<int> operators;
         vector<string> result;
-        operators.emplace_back(0);
-        stack<int> nums;
-        stack<int> ops;
 
-        dfs(digits[0], digits, operators, nums, ops, result, target);
+        dfs(digits[0], stack<tuple<int, long, long>>(), digits, operators, result, target);
 
         return result;
     }
 
 private:
-    void dfs(int sum, const vector<int> &digits, list<int> &operators, stack<int> nums, stack<int> ops, vector<string> &result, int target)
+    void dfs(long value, stack<tuple<int, long, long>>& state, const vector<int> &digits, list<int> &operators, vector<string> &result, int target)
     {
         if (operators.size() < digits.size() - 1)
         {
             for (int i = 0; i < 4; i++)
             {
-                int t_sum = sum;
-                stack<int> t_nums = nums;
-                stack<int> t_ops = ops;
+                long t_value = 0;
+                stack<tuple<int, long, long>> t_state = state;
+                try
+                {
+                    t_value = eval(value, t_state, i, digits, operators);
+                }
+                catch (const std::exception &)
+                {
+                    continue;
+                }
                 operators.emplace_back(i);
-                int right = digits[i + 1];
-                if (operators.back() == 0)
-                {
-                    if (t_sum == 0)
-                    {
-                        operators.pop_back();
-                        continue;
-                    }
-                    t_sum = t_sum * 10 + right;
-                }
-                else if (operators.back() == 3)
-                {
-                    while (!ops.empty() && ops.top() == 3)
-                    {
-                        t_sum *= nums.top();
-                        t_ops.pop();
-                        t_nums.pop();
-                    }
-
-                    t_nums.emplace(t_sum);
-                    t_ops.emplace(operators.back());
-                }
-                else
-                {
-                    while (!t_ops.empty())
-                    {
-                        switch (t_ops.top())
-                        {
-                        case 1:
-                            t_sum += t_nums.top();
-                            break;
-                        case 2:
-                            t_sum = t_nums.top() - t_sum;
-                            break;
-                        case 3:
-                            t_sum *= t_nums.top();
-                            break;
-
-                        default:
-                            break;
-                        }
-                        t_ops.pop();
-                        t_nums.pop();
-                    }
-
-                    t_nums.emplace(t_sum);
-                    t_ops.emplace(operators.back());
-                }
-                dfs(t_sum, digits, operators, t_nums, t_ops, result, target);
+                dfs(t_value, t_state, digits, operators, result, target);
                 operators.pop_back();
             }
         }
         else
         {
-            int temp = sum;
-            while (!ops.empty())
+            if (value == target)
             {
-                switch (ops.top())
-                {
-                case 1:
-                    temp += nums.top();
-                    break;
-                case 2:
-                    temp = nums.top() - temp;
-                    break;
-                case 3:
-                    temp *= nums.top();
-                    break;
-                default:
-                    break;
-                }
-                ops.pop();
-                nums.pop();
-            }
-            if (temp == target)
-            {
+                // string temp = toString(digits, operators);
+                // assert(temp != "1*2+3*4*5-6*7+8+9");
                 result.emplace_back(toString(digits, operators));
             }
-            operators.pop_back();
+        }
+    }
+
+    long eval(long value, stack<tuple<int, long, long>>& state, int op, const vector<int> &digits, const list<int> &operators)
+    {
+        while (!state.empty() && ((get<0>(state.top()) > op || op == 1))
+        {
+            state.pop();
+        }
+
+        long *left = nullptr;
+        if (!state.empty())
+        {
+            left = &get<2>(state.top());
+        }
+        else
+        {
+            left = &value;
+        }
+        long right = digits[operators.size() + 1];
+        *left = calc(*left, right, op);
+
+        if (state.empty())
+        {
+            state.emplace(op, *left, right);
+        }
+        else
+        {
+            get<2>(state.top()) = *left;
+        }
+        
+    }
+
+    long calc(long left, long right, int op)
+    {
+        switch (op)
+        {
+        case 0:
+            return left + right;
+            break;
+        case 1:
+            return left - right;
+            break;
+        case 2:
+            return left * right;
+            break;
+        default:
+        {
+            if (left == 0)
+            {
+                throw exception();
+            }
+            else
+            {
+                return left * 10 + right;
+            }
+        }
+        break;
         }
     }
 
@@ -146,12 +141,12 @@ private:
             string op;
             switch (*(it++))
             {
-            case 0:
+            case 3:
                 break;
-            case 1:
+            case 0:
                 op = "+";
                 break;
-            case 2:
+            case 1:
                 op = "-";
                 break;
             default:
@@ -162,7 +157,6 @@ private:
         }
         return result.str();
     }
-
 };
 
 void Test()
@@ -170,18 +164,20 @@ void Test()
     Solution solution;
     vector<string> output;
 
-    output = {"1+2+3", "1*2*3"};
-    assert(solution.addOperators("123", 6) == output);
+    // output = {"1+2+3", "1*2*3"};
+    // assert(solution.addOperators("123", 6) == output);
 
-    output = {"2*3+2", "2+3*2"};
-    assert(solution.addOperators("232", 8) == output);
+    // output = {"2+3*2", "2*3+2"};
+    // assert(solution.addOperators("232", 8) == output);
 
-    output = {"1*0+5", "10-5"};
-    assert(solution.addOperators("105", 5) == output);
+    // output = {"1*0+5", "10-5"};
+    // assert(solution.addOperators("105", 5) == output);
 
-    output = {"0+0", "0-0", "0*0"};
-    assert(solution.addOperators("00", 0) == output);
+    // output = {"0+0", "0-0", "0*0"};
+    // assert(solution.addOperators("00", 0) == output);
 
-    output = {};
-    assert(solution.addOperators("3456237490", 9191) == output);
+    // output = {};
+    // assert(solution.addOperators("3456237490", 9191) == output);
+
+    solution.addOperators("123456789", 45);
 }
