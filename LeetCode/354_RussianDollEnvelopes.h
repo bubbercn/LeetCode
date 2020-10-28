2a7dd234072ad433f2bae070f9ad59541b4b9aa1
Baosong Wu <bubbercn@163.com>
2020-10-28 08:40:22 +0800

    354 work in-progress.

--- LeetCode/354_RussianDollEnvelopes.h
+++ LeetCode/354_RussianDollEnvelopes.h
@@ -7,8 +7,33 @@ public:
     int maxEnvelopes(vector<vector<int>> &envelopes)
     {
         sort(envelopes.begin(), envelopes.end());
-
-        return 0;
+        int max = 0;
+        stack<vector<int>> increasingSequence;
+        for (auto it = envelopes.begin(); it != envelopes.end();)
+        {
+            auto curIt = it;
+            map<int, vector<vector<int>>::const_iterator> lookup;
+            while (it != envelopes.end() && it->at(0) == curIt->at(0))
+            {
+                lookup.emplace(it->at(1), it);
+                it++;
+            }
+            auto i = lookup.begin();
+            while (!increasingSequence.empty() && (i = lookup.upper_bound(increasingSequence.top()[1])) == lookup.end())
+            {
+                increasingSequence.pop();
+            }
+            if (i != lookup.end())
+            {
+                increasingSequence.emplace(*(i->second));
+            }
+            else
+            {
+                increasingSequence.emplace(*curIt);
+            }
+            max = ::max(max, static_cast<int>(increasingSequence.size()));
+        }
+        return max;
     }
 };
 
@@ -21,5 +46,17 @@ public:
 TEST_F(LeetCodeTest, Example1)
 {
     vector<vector<int>> input = {{5, 4}, {6, 4}, {6, 7}, {2, 3}};
+    EXPECT_EQ(solution.maxEnvelopes(input), 3);
+}
+
+TEST_F(LeetCodeTest, Failure1)
+{
+    vector<vector<int>> input = {{30, 50}, {12, 2}, {3, 4}, {12, 15}};
+    EXPECT_EQ(solution.maxEnvelopes(input), 3);
+}
+
+TEST_F(LeetCodeTest, Failure2)
+{
+    vector<vector<int>> input = {{46, 89}, {50, 53}, {52, 68}, {72, 45}, {77, 81}};
     EXPECT_EQ(solution.maxEnvelopes(input), 3);
 }
