d16324aeed498fdb450054f00c3c3481bf162433
Baosong Wu <bawu@microstrategy.com>
2020-12-17 19:17:15 +0800

    381 work in-progress.

--- LeetCode/381_InsertDeleteGetRandomO(1)DuplicatesAllowed.h
+++ LeetCode/381_InsertDeleteGetRandomO(1)DuplicatesAllowed.h
@@ -12,13 +12,24 @@ public:
     /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     bool insert(int val)
     {
-        return false;
+        bool notExist = index.find(val) == index.end()
+        index.emplace(val, values.size());
+        values.emplace_back(val);
+        return notExist;
     }
 
     /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     bool remove(int val)
     {
-        return false;
+        auto it = index.find(val);
+        if (it == index.end())
+            return false;
+        
+        values[it->second] = values.back();
+        index[values.back()] = it->second;
+        index.erase(it);
+        values.pop_back();
+        return true;
     }
 
     /** Get a random element from the collection. */
@@ -26,6 +37,10 @@ public:
     {
         return 0;
     }
+
+private:
+    unordered_multimap<int, int> index;
+    vector<int> values;
 };
 
 class LeetCodeTest : public testing::Test
