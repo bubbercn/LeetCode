542673755115333d75b19bac46e6a9862799731d
Baosong Wu <Bubbercn@163.com>
2021-01-01 15:23:33 +0800

    395 work in-progress.

--- LeetCode/395_LongestSubstringWithAtLeastKRepeatingCharacters.h
+++ LeetCode/395_LongestSubstringWithAtLeastKRepeatingCharacters.h
@@ -6,7 +6,43 @@ class Solution
 public:
     int longestSubstring(string s, int k)
     {
-        return 0;
+        vector<int> lookup(26, 0);
+        for (auto c : s)
+        {
+            lookup[c - 'a']++;
+        }
+
+        int result = INT_MIN;
+        int begin = -1;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (lookup[s[i] - 'a'] < k)
+            {
+                if (begin == -1)
+                {
+                    result = max(result, 0);
+                }
+                else
+                {
+                    result = max(result, longestSubstring(s.substr(begin, i - begin), k));
+                    begin = -1;
+                }                
+            }
+            else
+            {
+                if (begin == -1)
+                {
+                    begin = i;
+                }
+            }
+        }
+
+        if (begin != -1)
+        {
+            result = max(result, longestSubstring(s.substr(begin), k));
+        }
+
+        return result;
     }
 };
 
@@ -24,4 +60,9 @@ TEST_F(LeetCodeTest, Example1)
 TEST_F(LeetCodeTest, Example2)
 {
     EXPECT_EQ(solution.longestSubstring("ababbc", 2), 5);
+}
+
+TEST_F(LeetCodeTest, Failure1)
+{
+    EXPECT_EQ(solution.longestSubstring("ababacb", 3), 0);
 }
