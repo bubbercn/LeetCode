7d4f77a7c4132b51df9fb51d3a8af3ba31b807cd
Baosong Wu <bawu@microstrategy.com>
2021-02-07 18:54:52 +0800

    407 work in-progress.

--- LeetCode/407_TrappingRainWaterII.h
+++ LeetCode/407_TrappingRainWaterII.h
@@ -6,7 +6,44 @@ class Solution
 public:
     int trapRainWater(vector<vector<int>> &heightMap)
     {
-        return 0;
+        assert(!heightMap.empty());
+        int rowCount = heightMap.size();
+        int columnCount = heightMap[0].size();
+        vector<vector<bool>> visited(rowCount, vector<bool>(columnCount, false));
+
+        auto cmp = [&heightMap](const pair<int, int> &left, const pair<int, int> &right) {
+            return heightMap[left.second][left.second] < heightMap[right.first][right.second];
+        };
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> heap(cmp);
+
+        auto insertToHeap = [&visited, &heap] (int row, int column) {
+            if (!visited[row][column])
+            {
+                heap.emplace(row, column);
+                visited[row][column] = true;
+            }
+        };
+
+        for (int j = 0; j < columnCount; j++)
+        {
+            insertToHeap(0, j);
+            insertToHeap(rowCount - 1, j);
+        }
+
+        for (int i = 0; i < columnCount; i++)
+        {
+            insertToHeap(i, 0);
+            insertToHeap(i, columnCount - 1);
+        }
+
+        int result = 0;
+        while (!heap.empty())
+        {
+            
+        }
+
+        return result;
     }
 };
 
