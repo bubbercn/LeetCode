a3da208791430725ff1cc178c52ce8f2f9311562
bubbercn <bubbercn@163.com>
2021-04-11 06:54:20 +0800

    440 work in-progress.

--- LeetCode/440_KthSmallestInLexicographicalOrder.h
+++ LeetCode/440_KthSmallestInLexicographicalOrder.h
@@ -6,7 +6,71 @@ class Solution
 public:
     int findKthNumber(int n, int k)
     {
-        return 0;
+        int depth = 0;
+        for (int i = n; i > 0; i /= 10)
+        {
+            depth++;
+        }
+        string result = helper(to_string(n), depth, k, false);
+        return stoi(result);
+    }
+
+private:
+    string helper(const string &limit, int depth, int &k, bool isCallByItself = true, bool updateK = true)
+    {
+        if (k == 0)
+            return {};
+        char start = isCallByItself ? '0' : '1';
+        if (depth == 1)
+        {
+            if (!limit.empty() && k <= limit[0] - '0' + 1)
+            {
+                int temp = k;
+                k = 0;
+                return string(1, start + temp - 1);
+            }
+            else
+            {
+                k -= limit[0] - '0' + 1;
+                return {};
+            }
+        }
+        int nodesCount = 0;
+
+        for (int i = 0, j = 1; i < depth; i++, j *= 10)
+        {
+            nodesCount += j;
+        }
+        char c = start;
+        int temp = k;
+        for (;temp > nodesCount; c++)
+        {
+            temp -= nodesCount;
+            if (updateK)
+            {
+                k = temp;
+            }
+            else
+            {
+                c++;
+            }
+        }
+        if (!limit.empty() && c == limit[0])
+        {
+            string temp = helper(limit.substr(1), depth - 1, --k);
+            if (k != 0)
+            {
+                return string{++c} + helper(limit, depth, k, true, false);
+            }
+            else
+            {
+                return string({c}) + temp;
+            }
+        }
+        else
+        {
+            return string({c}) + helper("", depth - 1, --k);
+        }
     }
 };
 
@@ -19,4 +83,29 @@ public:
 TEST_F(LeetCodeTest, Example1)
 {
     EXPECT_EQ(solution.findKthNumber(13, 2), 10);
+}
+
+TEST_F(LeetCodeTest, Example2)
+{
+    EXPECT_EQ(solution.findKthNumber(1, 1), 1);
+}
+
+TEST_F(LeetCodeTest, Case1)
+{
+    EXPECT_EQ(solution.findKthNumber(9, 2), 2);
+}
+
+TEST_F(LeetCodeTest, Case2)
+{
+    EXPECT_EQ(solution.findKthNumber(900, 112), 2);
+}
+
+TEST_F(LeetCodeTest, Failure1)
+{
+    EXPECT_EQ(solution.findKthNumber(10, 3), 2);
+}
+
+TEST_F(LeetCodeTest, Failure2)
+{
+    EXPECT_EQ(solution.findKthNumber(100, 10), 17);
 }
