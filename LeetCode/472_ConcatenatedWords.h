a67794c5c182a8477c181234f5ce7f6f68f50da6
bubbercn <bubbercn@163.com>
2021-06-06 16:23:48 +0800

    472 work in-progress.

--- LeetCode/472_ConcatenatedWords.h
+++ LeetCode/472_ConcatenatedWords.h
@@ -6,7 +6,74 @@ class Solution
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string> &words)
     {
-        return {};
+        auto cmp = [](const string &s1, const string &s2)
+        {
+            if (s1.length() == s2.length())
+                return s1 < s2;
+            return s1.length() < s2.length();
+        };
+        sort(words.begin(), words.end(), cmp);
+        DictionaryNode root{false, {}};
+        vector<string> result;
+        for (const auto &word : words)
+        {
+            DictionaryNode *parent = &root;
+            bool isConcatenated = false;
+            for (int i = 0; i < word.length(); i++)
+            {
+                auto [it, inserted] = parent->children.emplace(word[i], nullptr);
+                if (inserted)
+                {
+                    it->second.reset(new DictionaryNode{i == word.length() - 1, {}});
+                }
+                else
+                {
+                    if (!isConcatenated && it->second.get()->exitWord)
+                    {
+                        isConcatenated = Solution::isConcatenated(&root, word.substr(i + 1));
+                        if (isConcatenated)
+                            result.emplace_back(word);
+                    }
+                }
+                parent = it->second.get();
+            }
+        }
+        return result;
+    }
+
+private:
+    struct DictionaryNode
+    {
+        bool exitWord;
+        unordered_map<char, unique_ptr<DictionaryNode>> children;
+    };
+    bool isConcatenated(DictionaryNode *root, const string &word)
+    {
+        static unordered_map<string, bool> lookup;
+        if (auto i = lookup.find(word); i != lookup.end())
+        {
+            return i->second;
+        }
+        DictionaryNode* parent = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (auto it = parent->children.find(word[i]); it == parent->children.end())
+            {
+                lookup[word] = false;
+                return false;
+            }
+            else
+            {
+                if (it->second.get()->exitWord && (i == word.length() - 1 || isConcatenated(root, word.substr(i + 1))))
+                {
+                    lookup[word] = true;
+                    return true;
+                }
+                parent = it->second.get();
+            }
+        }
+        lookup[word] = true;
+        return true;
     }
 };
 
