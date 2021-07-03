75d53eecfa852193eff1c5a0efa56de01273e50a
bubbercn <bubbercn@163.com>
2021-07-03 18:38:55 +0800

    480 work in-progress.

--- LeetCode/480_SlidingWindowMedian.h
+++ LeetCode/480_SlidingWindowMedian.h
@@ -6,7 +6,71 @@ class Solution
 public:
     vector<double> medianSlidingWindow(vector<int> &nums, int k)
     {
-        return {};
+        priority_queue<int> maxHeap;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        multiset<int> removedMax;
+        multiset<int> removedMin;
+        int k2 = k / 2;
+
+        for (int i = 0; i < k; i++)
+        {
+            if (maxHeap.size() <= k2)
+            {
+                maxHeap.emplace(nums[i]);
+            }
+            else if (nums[i] >= maxHeap.top())
+            {
+                minHeap.emplace(nums[i]);
+            }
+            else
+            {
+                maxHeap.emplace(nums[i]);
+                minHeap.emplace(maxHeap.top());
+                maxHeap.pop();
+            }
+        }
+
+        vector<double> result;
+        result.reserve(nums.size() + 1 - k);
+        bool flag = maxHeap.size() > minHeap.size();
+        auto pushResult = [&]()
+        {
+            if (flag)
+            {
+                result.emplace_back(maxHeap.top());
+            }
+            else
+            {
+                result.emplace_back((maxHeap.top() + minHeap.top()) / 2.0);
+            }
+        };
+        pushResult();
+
+        for (int i = k; i < nums.size(); i++)
+        {
+            if (nums[i] >= maxHeap.top())
+            {
+                minHeap.emplace(nums[i]);
+                removedMin.emplace(nums[i - k]);
+            }
+            else
+            {
+                maxHeap.emplace(nums[i]);
+                removedMax.emplace(nums[i - k]);
+            }
+            for (auto it = removedMax.find(maxHeap.top()); it != removedMax.end(); it = removedMax.find(maxHeap.top()))
+            {
+                maxHeap.pop();
+                removedMax.erase(it);
+            }
+            for (auto it = removedMin.find(minHeap.top()); it != removedMin.end(); it = removedMin.find(minHeap.top()))
+            {
+                minHeap.pop();
+                removedMin.erase(it);
+            }
+            pushResult();
+        }
+        return result;
     }
 };
 
