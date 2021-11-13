3dff1beabcb72eda521a3e0d441eb863d67d9c61
bubbercn <bubbercn@163.com>
2021-11-13 22:13:13 +0800

    583 work in-progress.

--- 583_DeleteOperationForTwoStrings.h
+++ 583_DeleteOperationForTwoStrings.h
@@ -6,7 +6,37 @@ class Solution
 public:
     int minDistance(string_view word1, string_view word2)
     {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < word1.length(); i++)
+        {
+            auto pos = word2.find(word1[i]);
+            if (pos == word1.npos)
+            {
+                result++;
+            }
+            else
+            {
+                result += pos;
+
+                if (pos + 1 == word2.length())
+                {
+                    result += word1.length() - i - 1;
+                    return result;
+                }
+                
+                if (i + 1 == word1.length())
+                {
+                    result += word2.length() - pos - 1;
+                    return result;
+                    
+                }
+                
+                result += min(minDistance(word1, word2.substr(pos + 1)), minDistance(word1.substr(i + 1), word2.substr(pos + 1)));
+                return result;
+            }
+        }
+        result += word2.length();
+        return result;
     }
 };
 
@@ -24,4 +54,24 @@ TEST_F(LeetCodeTest, Example1)
 TEST_F(LeetCodeTest, Example2)
 {
     EXPECT_EQ(solution.minDistance("leetcode", "etco"), 4);
+}
+
+TEST_F(LeetCodeTest, Case1)
+{
+    EXPECT_EQ(solution.minDistance("a", "b"), 2);
+}
+
+TEST_F(LeetCodeTest, Failure1)
+{
+    EXPECT_EQ(solution.minDistance("sea", "ate"), 4);
+}
+
+TEST_F(LeetCodeTest, Failure2)
+{
+    EXPECT_EQ(solution.minDistance("mart", "karma"), 5);
+}
+
+TEST_F(LeetCodeTest, Failure3)
+{
+    EXPECT_EQ(solution.minDistance("intention", "execution"), 8);
 }
