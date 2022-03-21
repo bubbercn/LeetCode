098f35ba827cee65d7fccfade1ada197ece2a6c9
bubbercn <bubbercn@163.com>
2022-03-21 21:52:26 +0800

    715 work in-progress.

--- 715_RangeModule.h
+++ 715_RangeModule.h
@@ -10,16 +10,67 @@ public:
 
     void addRange(int left, int right)
     {
+        if (queryRange(left, right))
+            return;
+
+        auto leftLower = leftMap.lower_bound(left);
+        auto leftUpper = leftMap.upper_bound(right);
+        for (auto it = leftLower; it != leftUpper;)
+        {
+            right = max(right, it->second);
+            rightMap.erase(it->second);
+            it = leftMap.erase(it);
+        }
+        auto rightLower = rightMap.lower_bound(left);
+        auto rightUpper = rightMap.upper_bound(right);
+        for (auto it = rightLower; it != rightUpper;)
+        {
+            int leftTemp = it->second->first;
+            left = min(left, leftTemp);
+            it = rightMap.erase(it);
+            leftMap.erase(leftTemp);
+        }
+        auto it = leftMap.emplace(left, right).first;
+        rightMap.emplace(right, it);
     }
 
     bool queryRange(int left, int right)
     {
-        return false;
+        if (leftMap.empty())
+            return false;
+        auto lower = leftMap.lower_bound(left);
+        if (lower != leftMap.end() && lower->first > left && lower == leftMap.begin())
+            return false;
+        lower--;
+        return lower->second >= right;
     }
 
     void removeRange(int left, int right)
     {
+        auto leftLower = leftMap.lower_bound(left);
+        auto leftUpper = leftMap.upper_bound(right);
+        for (auto it = leftLower; it != leftUpper;)
+        {
+            right = max(right, it->second);
+            rightMap.erase(it->second);
+            it = leftMap.erase(it);
+        }
+        auto rightLower = rightMap.lower_bound(left);
+        auto rightUpper = rightMap.upper_bound(right);
+        for (auto it = rightLower; it != rightUpper;)
+        {
+            int leftTemp = it->second->first;
+            left = min(left, leftTemp);
+            it = rightMap.erase(it);
+            leftMap.erase(leftTemp);
+        }
+        auto it = leftMap.emplace(left, right).first;
+        rightMap.emplace(right, it);
     }
+
+private:
+    map<int, int> leftMap;
+    map<int, map<int, int>::const_iterator> rightMap;
 };
 
 class Solution
@@ -35,7 +86,10 @@ public:
 TEST_F(LeetCodeTest, Example1)
 {
     RangeModule rangeModule;
+    EXPECT_FALSE(rangeModule.queryRange(13, 15));
     rangeModule.addRange(10, 20);
+    EXPECT_FALSE(rangeModule.queryRange(9, 15));
+    EXPECT_TRUE(rangeModule.queryRange(13, 15));
     rangeModule.removeRange(14, 16);
     EXPECT_TRUE(rangeModule.queryRange(10, 14));
     EXPECT_FALSE(rangeModule.queryRange(13, 15));
