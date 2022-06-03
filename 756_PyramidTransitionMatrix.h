1149b5fc327b3788a40a06ef62802ce45de9d5a8
bubbercn <bubbercn@163.com>
2022-06-03 16:40:22 +0800

    756 work in-progress.

--- 756_PyramidTransitionMatrix.h
+++ 756_PyramidTransitionMatrix.h
@@ -4,10 +4,32 @@
 class Solution
 {
 public:
-    bool pyramidTransition(string bottom, vector<string> &allowed)
+    bool pyramidTransition(string_view bottom, vector<string> &allowed)
     {
+        int n = bottom.size();
+        pyramid.assign(n, {});
+        pyramid[n - 1] = bottom;
+        for (int i = 0; i < n - 1; i++)
+        {
+            pyramid[i].assign(i + 1, '0');
+        }
+        lookup.clear();
+        for (auto &rule : allowed)
+        {
+            lookup.emplace(rule.substr(0, 2), rule[2]);
+        }
+        return dfs(n - 1, 0);
+    }
+
+private:
+    vector<string> pyramid;
+    unordered_map<string, char> lookup;
+    bool dfs(int floor, int start)
+    {
+        
         return false;
     }
+    bool help
 };
 
 class LeetCodeTest : public testing::Test
