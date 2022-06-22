a042bfd82cc04ec4a5cb4f712699915d0baf61e2
Baosong Wu <bubbercn@163.com>
2022-06-22 19:10:49 +0800

    765 work in-progress.

--- 765_CouplesHoldingHands.h
+++ 765_CouplesHoldingHands.h
@@ -6,7 +6,45 @@ class Solution
 public:
     int minSwapsCouples(vector<int> &row)
     {
-        return 0;
+        int n = row.size();
+        unordered_map<int, int> person2SlotMap;
+        unordered_map<int, set<int>> slot2PersonMap;
+        for (int i = 0; i < n; i++)
+        {
+            int slot = i / 2;
+            person2SlotMap.emplace(row[i], slot);
+            slot2PersonMap[slot].emplace(row[i]);
+        }
+        vector<bool> visited(n, false);
+        int result = 0;
+        auto helper = [&](int person)
+        {
+            unordered_set<int> slots;
+            while (true)
+            {
+                visited[person] = true;
+                int slot = person2SlotMap[person];
+                int other = person % 2 == 0 ? person + 1 : person - 1;
+                visited[other] = true;
+                if (slots.emplace(slot).second)
+                {
+                    set<int>& couple = slot2PersonMap[person2SlotMap[other]] ;
+                    person = other == *couple.begin() ? *couple.rbegin() : *couple.begin();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return slots.size() - 1;
+        };
+        for (int i = 0; i < n; i++)
+        {
+            if (visited[i])
+                continue;
+            result += helper(i);
+        }
+        return result;
     }
 };
 
