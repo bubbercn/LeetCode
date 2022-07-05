451c5c867c4eb887e03436c1b3f9a08dc04a205b
Baosong Wu <bubbercn@163.com>
2022-07-05 18:29:43 +0800

    777 work in-progress.

--- 777_SwapAdjacentInLRString.h
+++ 777_SwapAdjacentInLRString.h
@@ -6,6 +6,59 @@ class Solution
 public:
     bool canTransform(string_view start, string_view end)
     {
+        int n = start.size();
+        int xIndex1 = -1, xIndex2 = -1;
+        for (int i = 0; i < n; ++i)
+        {
+            if (start[i] == end[i])
+                continue;
+
+            if (start[i] == 'X')
+            {
+                if (xIndex1 == -1)
+                {
+                    xIndex1 = i;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            if (end[i] == 'X')
+            {
+                if (xIndex2 == -1)
+                {
+                    xIndex2 = i;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            if (xIndex1 != -1 && xIndex2 != -1)
+            {
+                if (xIndex1 < xIndex2)
+                {
+                    if (start.substr(xIndex1 + 1, xIndex2 - xIndex1) != end.substr(xIndex1, xIndex2 - xIndex1))
+                        return false;
+                    
+                }
+                else
+                {
+
+                }
+                xIndex1 = -1;
+                xIndex2 = -1;
+            }
+            else if (xIndex1 != -1 || xIndex2 != -1)
+            {
+                return false;
+            }
+        }
+        if (xIndex1 == -1 && xIndex2 == -1)
+        {
+            return true;
+        }
         return false;
     }
 };
