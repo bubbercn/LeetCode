64ff8668160065e0e8b609d416edd44ea3c167ac
bubbercn <bubbercn@163.com>
2023-01-07 15:38:17 +0800

    854 work in-progress.

--- 854_KSimilarStrings.h
+++ 854_KSimilarStrings.h
@@ -6,7 +6,21 @@ class Solution
 public:
     int kSimilarity(string_view s1, string_view s2)
     {
-        return 0;
+        if (s1 == s2)
+            return 0;
+        
+        int result = numeric_limits<int>::max();
+        char c = s1[0];
+        for (int i = 0; i < s2.length(); i++)
+        {
+            if (s2[i] == c)
+            {
+                string temp(s2.substr(0, i));
+                temp += s2.substr(i + 1);
+                result = min(result, kSimilarity(s1.substr(1), temp) + 1);
+            }
+        }
+        return result;
     }
 };
 
@@ -23,5 +37,5 @@ TEST_F(LeetCodeTest, Example1)
 
 TEST_F(LeetCodeTest, Example2)
 {
-    EXPECT_EQ(solution.kSimilarity("abc", "cba"), 1);
+    EXPECT_EQ(solution.kSimilarity("abc", "cba"), 2);
 }
