0a03b4735ddd4ea909954902154c226ef0089b7a
Baosong Wu <bubbercn@163.com>
2023-09-16 12:01:58 +0800

    1003 work in-progress.

--- Java/1003_CheckIfWordIsValidAfterSubstitutions.java
+++ Java/1003_CheckIfWordIsValidAfterSubstitutions.java
@@ -1,5 +1,16 @@
 class Solution1003 {
     public boolean isValid(String s) {
-        return false;
+        StringBuffer sb = new StringBuffer(s);
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == 'c') {
+                if (i < 2)
+                    return false;
+                if (sb.charAt(i - 1) != 'b' || sb.charAt(i - 2) != 'a')
+                    return false;
+                sb.replace(i - 2, i + 1, "");
+                i -= 3;
+            }
+        }
+        return sb.isEmpty();
     }
 }
